0885a0795b736b525b4ea5f1a746a95aae4f971d
David Preece <davep@zedkep.com>
2018-08-15 14:47:46 +1200

    Revert "Revert "Making a list of available symbols instead of iterating through flags""
    This reverts commit 8d6e8493c9e9fecc3eaf8d812d9b721f9d005c1e.

--- sudoku/sudoku.hpp
+++ sudoku/sudoku.hpp
@@ -4,7 +4,7 @@
 #include <cstdint>
 #include <iostream>
 
-typedef uint8_t nine[9];
+typedef uint_fast8_t nine[9];
 
 // Takes input strings of the form...
 // 37........852....71..57.4.9..435.9.893.....258.1.276..2.3.94..65....219........42
@@ -17,32 +17,37 @@ public:
     void solve();
     
 private:
-    void dumpnine(const char* title, int index, const nine& nine, uint8_t symbols_used);
+    void dumpnine(const char* title, int index, const nine& nine, uint_fast8_t symbols_used);
     struct Block
     {
         nine positions;
-        nine symbols_used {false, false, false, false, false, false, false, false, false};
-        uint8_t n_symbols_used { 0 };
+        nine symbols_used { false, false, false, false, false, false, false, false, false };
+        uint_fast8_t n_symbols_used { 0 };
     };
     
     struct Line
     {
-        nine symbols_used {false, false, false, false, false, false, false, false, false};
-        uint8_t n_symbols_used { 0 };
+        nine symbols_used { false, false, false, false, false, false, false, false, false };
+        uint_fast8_t n_symbols_used { 0 };
     };
     
     struct Empty
     {
-        uint8_t position;
-        uint8_t known;
+        Empty() : possible(0), next_possible_to_try(0) {};
+        uint_fast8_t position;
+        uint_fast8_t possible;
+        uint_fast8_t next_possible_to_try;
+        nine possibles { true, true, true, true, true, true, true, true, true };
+        void eliminate_for(const nine& symbols);
+        void enlistify();
     };
     
     struct Geometry
     {
-        uint8_t row : 4;
-        uint8_t column : 4;
-        uint8_t block : 4;
-        uint8_t intra_block : 4;
+        uint_fast8_t row;
+        uint_fast8_t column;
+        uint_fast8_t block;
+        uint_fast8_t intra_block;
     };
     
     Line rows[9];
@@ -50,7 +55,7 @@ private:
     Block blocks[9];
     Geometry geometry[81];
     Empty empty_positions[81];
-    uint8_t n_empty_positions { 0 };
+    uint_fast8_t n_empty_positions { 0 };
 };
 
 #endif /* sudoku_hpp */
